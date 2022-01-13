e5a81a4622bac0e4bf3fbe55edd8d7516097cdad
sarahkydnc <sarah.kydnc@gmail.com>
2022-01-13 17:50:41 +0800

    Added date and time live updates

--- src/app.js
+++ src/app.js
@@ -1,14 +1,61 @@
+function formatDate(timestamp) {
+  let today = new Date(timestamp);
+  let hours = today.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = today.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = today.getDate();
+  let year = today.getFullYear();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[today.getDay()];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[today.getMonth()];
+
+  return `${date} ${month} ${year}, ${day}, ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
-  let temp = document.querySelector("#temp");
-  let roundedTemp = Math.round(response.data.main.temp);
-  temp.innerHTML = roundedTemp;
 
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
 
+  let dateTime = document.querySelector("#date-time");
+  dateTime.innerHTML = formatDate(response.data.dt * 1000);
+
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+
+  let temp = document.querySelector("#temp");
+  let roundedTemp = Math.round(response.data.main.temp);
+  temp.innerHTML = roundedTemp;
 
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
